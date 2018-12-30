48915b48f3d208b1cd88297431029a4647963146
Evgenii Shchemelev <e_shchemelev@wargaming.net>
2018-12-30 22:13:22 +0700

    fix

--- main.py
+++ main.py
@@ -1,64 +1,44 @@
-from PIL import Image, ImageDraw, ImageFont
+import json
+import ConfigParser
 import codecs
-
-DPI = 300
-
-if DPI == 300:
-    PAGE_SIZE = (2480, 3508)
-    FONT_SIZE = 27 * 2
-    TEXT_PADDING = 6
-    PLACEHOLDER_DIMENSIONS = 222
-    PLACEHOLDER_SIZE = (PLACEHOLDER_DIMENSIONS, PLACEHOLDER_DIMENSIONS)
-    PLACEHOLDER_PADDING = 13
-    OUTLINE_IN_PIXELS = 2
-    TOP_PADDING = 50
-    LEFT_PADDING = 50
-    RIGHT_PADDING = 30
-    NUMBER_IN_ROW = (2480 - LEFT_PADDING - RIGHT_PADDING) / (PLACEHOLDER_PADDING + PLACEHOLDER_DIMENSIONS)
-    DPI = (300, 300)
-else:
-    PAGE_SIZE = (1240, 1754)
-    FONT_SIZE = 27
-    TEXT_PADDING = 3
-    PLACEHOLDER_DIMENSIONS = 111
-    PLACEHOLDER_SIZE = (PLACEHOLDER_DIMENSIONS, PLACEHOLDER_DIMENSIONS)
-    PLACEHOLDER_PADDING = 14
-    OUTLINE_IN_PIXELS = 1
-    TOP_PADDING = 50
-    LEFT_PADDING = 50
-    RIGHT_PADDING = 30
-    NUMBER_IN_ROW = (1240 - LEFT_PADDING - RIGHT_PADDING) / (PLACEHOLDER_PADDING + PLACEHOLDER_DIMENSIONS)
-    DPI = (150, 150)
-
-background_color = (256, 256, 256)
-font_color = (0, 0, 0)
-shadow_color = (255, 255, 255)
-
-main_image = Image.new('RGB', PAGE_SIZE, background_color)
-
-font = ImageFont.truetype("impact.ttf", FONT_SIZE)
+from PIL import Image, ImageDraw, ImageFont
 
 
-def draw_outlined_text(draw, x, y, line, font, font_color):
+TOP_PADDING = 50
+LEFT_PADDING = 50
+RIGHT_PADDING = 30
+
+BACKGROUND_COLOR = (256, 256, 256)
+PAGE_SIZE = (2480, 3508)
+
+PLACEHOLDER_DIMENSIONS = 222
+PLACEHOLDER_PADDING = 13
+PLACEHOLDER_SIZE = (PLACEHOLDER_DIMENSIONS, PLACEHOLDER_DIMENSIONS)
+
+NUMBER_IN_ROW = (2480 - LEFT_PADDING - RIGHT_PADDING) / (PLACEHOLDER_PADDING + PLACEHOLDER_DIMENSIONS)
+
+TEXT_PADDING = 6
+
+
+def draw_outlined_text(draw, x, y, line, font):
     # move right
-    draw.text((x - OUTLINE_IN_PIXELS, y), line, font=font, fill=shadow_color)
+    draw.text((x - font.outline_size, y), line, font=font, fill=font.outline_color)
     # move left
-    draw.text((x + OUTLINE_IN_PIXELS, y), line, font=font, fill=shadow_color)
+    draw.text((x + font.outline_size, y), line, font=font, fill=font.outline_color)
     # move up
-    draw.text((x, y + OUTLINE_IN_PIXELS), line, font=font, fill=shadow_color)
+    draw.text((x, y + font.outline_size), line, font=font, fill=font.outline_color)
     # move down
-    draw.text((x, y - OUTLINE_IN_PIXELS), line, font=font, fill=shadow_color)
+    draw.text((x, y - font.outline_size), line, font=font, fill=font.outline_color)
     # diagnal left up
-    draw.text((x - OUTLINE_IN_PIXELS, y + OUTLINE_IN_PIXELS), line, font=font, fill=shadow_color)
+    draw.text((x - font.outline_size, y + font.outline_size), line, font=font, fill=font.outline_color)
     # diagnal right up
-    draw.text((x + OUTLINE_IN_PIXELS, y + OUTLINE_IN_PIXELS), line, font=font, fill=shadow_color)
+    draw.text((x + font.outline_size, y + font.outline_size), line, font=font, fill=font.outline_color)
     # diagnal left down
-    draw.text((x - OUTLINE_IN_PIXELS, y - OUTLINE_IN_PIXELS), line, font=font, fill=shadow_color)
+    draw.text((x - font.outline_size, y - font.outline_size), line, font=font, fill=font.outline_color)
     # diagnal right down
-    draw.text((x + OUTLINE_IN_PIXELS, y - OUTLINE_IN_PIXELS), line, font=font, fill=shadow_color)
+    draw.text((x + font.outline_size, y - font.outline_size), line, font=font, fill=font.outline_color)
     # main text
-    draw.text((x, y), line, font=font, fill=font_color)
-
+    draw.text((x, y), line, font=font, fill=font.color)
 
 
 def put_text_on_lense_image(im_obj, lines_array, font):
@@ -67,7 +47,7 @@ def put_text_on_lense_image(im_obj, lines_array, font):
     line_count = len(lines_array)
     if line_count == 3:
         t_padding = TEXT_PADDING/3
-        initial_v_offset = 24
+        initial_v_offset = 10
     else:
         t_padding = TEXT_PADDING
         initial_v_offset = (PLACEHOLDER_DIMENSIONS - line_count * (line_w + t_padding) - t_padding) / 2
@@ -81,37 +61,83 @@ def put_text_on_lense_image(im_obj, lines_array, font):
         h_offset = (PLACEHOLDER_DIMENSIONS - w) / 2
         v_offset = initial_v_offset + index * (h + t_padding)
 
-        draw_outlined_text(imgDrawer, h_offset, v_offset, stripped_line, font, font_color)
+        draw_outlined_text(imgDrawer, h_offset, v_offset, stripped_line, font)
 
 
-counter = 0
-config = codecs.open('config.txt', 'r', 'utf-8')
-for item in config.readlines():
-    if item:
+def load_image(filename_without_ext):
+    for ext in ['.png', '.jpg', 'jpeg']:
         try:
-            name, number, text  = item.split('|')
-            try:
-                number = int(number)
-            except:
-                number = 1
+            lense_image = Image.open('./images/'+filename_without_ext+ext).convert('RGBA')
+            return lense_image.resize(PLACEHOLDER_SIZE, Image.ANTIALIAS)
         except:
-            name = item.strip()
-            number = 1
-            text = ''
-        for i in range(number):
-            v_offset, h_offset = 0, 0
-            v_offset = counter / NUMBER_IN_ROW * (PLACEHOLDER_DIMENSIONS + PLACEHOLDER_PADDING) + TOP_PADDING
-            h_offset = counter % NUMBER_IN_ROW * (PLACEHOLDER_DIMENSIONS + PLACEHOLDER_PADDING) + LEFT_PADDING
-            try:
-                lense_image = Image.open('./images/'+name+'.png')
-                resized_image = lense_image.resize(PLACEHOLDER_SIZE, Image.ANTIALIAS)
-                lines_array = list(text.split(';'))
-            except:
-                resized_image = Image.new("RGB", PLACEHOLDER_SIZE, color='red')
-                lines_array = ['image', 'not', 'found']
-            put_text_on_lense_image(resized_image, lines_array, font)
-            main_image.paste(resized_image, (h_offset, v_offset))
-            counter += 1
+            pass
+    raise ValueError('file not found')
 
-main_image.show()
-main_image.save('output.png', dpi=DPI)
+
+def get_image_offset(image_number):
+    v_offset = image_number / NUMBER_IN_ROW * (PLACEHOLDER_DIMENSIONS + PLACEHOLDER_PADDING) + TOP_PADDING
+    h_offset = image_number % NUMBER_IN_ROW * (PLACEHOLDER_DIMENSIONS + PLACEHOLDER_PADDING) + LEFT_PADDING
+    return (h_offset, v_offset)
+
+
+def parse_input_attrs(line):
+    try:
+        name, number, text  = item.split('|')
+        try:
+            number = int(number)
+        except:
+            number = 1
+    except:
+        name = item.strip()
+        number = 1
+        text = ''
+    return name, number, text
+
+
+def get_print_config(config_filename):
+    f = open(config_filename, 'r')
+    result = dict()
+    for item in f.readlines():
+        name, value = item.strip().split('=')
+        try:
+            value = int(value)
+        except:
+            pass
+        try:
+            value = json.loads(value)
+        except:
+            pass
+        result[name] = value
+    return result
+
+
+if __name__ == '__main__':
+    counter = 0
+    lenses_list_for_print = codecs.open('lenses_list_for_print.txt', 'r', 'utf-8')
+    config = ConfigParser.RawConfigParser()
+    config.read('config.txt')
+
+    font = ImageFont.truetype(config.get('FONT', 'filename'), config.getint('FONT','size'))
+    font.outline_size = config.getint('FONT', 'outline_size')
+    font.outline_color = tuple(json.loads(config.get('FONT', 'outline_color')))
+    font.color = tuple(json.loads(config.get('FONT', 'color')))
+
+    main_image = Image.new('RGB', PAGE_SIZE, BACKGROUND_COLOR)
+
+    for item in lenses_list_for_print.readlines():
+        if item.strip():
+            name, number, text = parse_input_attrs(item)
+            for i in range(number):
+                try:
+                    resized_image = load_image(name)
+                    lines_array = list(text.split(';'))
+                except:
+                    resized_image = Image.new("RGBA", PLACEHOLDER_SIZE, color='red')
+                    lines_array = ['image', 'not', 'found']
+                put_text_on_lense_image(resized_image, lines_array, font)
+                # main_image.alpha_composite(resized_image, get_image_offset(counter))
+                main_image.paste(resized_image, get_image_offset(counter), resized_image)
+                counter += 1
+
+    main_image.show()
+    main_image.save('output.png', dpi=(300,300))
